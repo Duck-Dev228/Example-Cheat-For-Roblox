58af9d3fb43e7bf46103ff6af2b9f3b79b3d3031
Раб_шахбана <_@DESKTOP-IEAO252>
2021-11-28 12:43:47 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.inputscript = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-3, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Inject";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(132, 286);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Execute";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // inputscript
+            // 
+            this.inputscript.Location = new System.Drawing.Point(-3, 12);
+            this.inputscript.Name = "inputscript";
+            this.inputscript.Size = new System.Drawing.Size(251, 268);
+            this.inputscript.TabIndex = 2;
+            this.inputscript.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(-3, 330);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(251, 32);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(-3, 368);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(251, 28);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Btools";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(247, 397);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.inputscript);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox inputscript;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using WeAreDevs_API;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        readonly ExploitAPI api = new ExploitAPI();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (api.isAPIAttached())
+            {
+
+            }
+            else
+            {
+                if (File.Exists("Files\\exploit-main.dll"))
+                {
+                    File.Delete("Files\\exploit-main.dll");
+                }
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (api.isAPIAttached())
+            {
+                string input = inputscript.Text;
+                api.SendLuaScript(input);
+
+            }
+            else
+            {
+                MessageBox.Show("The Exploit is already Not Injected !");
+            }
+            
+        }
+           
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            inputscript.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (api.isAPIAttached())
+            {
+                MessageBox.Show("The exploit is already injected !");
+
+            }
+            else
+            {
+                api.LaunchExploit();
+
+                FileInfo gg = new FileInfo("exploit-main.dll");
+                gg.CopyTo("Files\\exploit-main.dll");
+                Thread.Sleep(100);
+                Thread.Sleep(200);
+                System.Diagnostics.Process fd = new System.Diagnostics.Process();
+                fd.StartInfo.FileName = "Files\\Injector.exe";
+                Thread.Sleep(100);
+                fd.Start();
+
+            }
+            
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            api.DoBTools();
+        }
+    }
+}
